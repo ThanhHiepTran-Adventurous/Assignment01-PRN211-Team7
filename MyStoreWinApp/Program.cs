320e3053d68940e91edb7267bd92a8579e842ade
oggy-learn-code <vunguyenhuychuong10t2@gmail.com>
2022-07-10 00:01:41 +0700

    Design Login Form

--- MyStoreWinApp/Program.cs
+++ MyStoreWinApp/Program.cs
@@ -1,16 +1,28 @@
+
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Windows.Forms;
+
+
 namespace MyStoreWinApp
 {
     internal static class Program
     {
+
+        public static IConfiguration Configuration;
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            Configuration = builder.Build();
+
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmLogin());
         }
     }
--- MyStoreWinApp/frmLogin.cs
+++ MyStoreWinApp/frmLogin.cs
@@ -1,10 +1,35 @@
+using Microsoft.Extensions.Configuration;
+
+using DataAccess.Repository;
 namespace MyStoreWinApp
 {
     public partial class frmLogin : Form
-    {
+    {   
+
+        IMemberRepository memberRepository = new IMemberRepository();
         public frmLogin()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            var adminLogin = Program.Configuration.GetSection("AdminAccount").Get<MemberObject>();
+
+
+            var mem = MemberDAO.Instance.GetMemberList();
+            string Email = adminLogin.Email;
+
+            string Password = adminLogin.Password;
+
+
+            string _Email = txtEmail.Text;
+            string _Password = txtPassword.Text;
+
+
+            string EmailMemberTest = txtEmail.Text;
+            string PasswordMemberTest = txtPassword.Text;
+            bool checkLogin = false;
         }
     }
 }
